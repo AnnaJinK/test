c7a9cb8ddaa1d959c0476503613b6b88bebe3319
Robotry <34006003+AnnaJinK@users.noreply.github.com>
2020-08-24 18:11:47 +0900

    Updates

---
+++ precache-manifest.121fb3c3f3d10162d2d59c5fe36209de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecad00e4dbe9fb7bf7669d9a250d99d6",
+    "url": "/robotry-entrypage/index.html"
+  },
+  {
+    "revision": "33db02562cf5c5e54bc8",
+    "url": "/robotry-entrypage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c280e42064c253438680",
+    "url": "/robotry-entrypage/static/js/2.bcf738bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotry-entrypage/static/js/2.bcf738bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33db02562cf5c5e54bc8",
+    "url": "/robotry-entrypage/static/js/main.b297744d.chunk.js"
+  },
+  {
+    "revision": "855c29b7c18aaedc9068",
+    "url": "/robotry-entrypage/static/js/runtime-main.8c8c5865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotry-entrypage/static/media/logo.5d5d9eef.svg"
+  }
+]);
